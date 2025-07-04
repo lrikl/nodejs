6aca53b8377bec2e9438c833f06523e8db8b0d7b
Yaroslav <lgalahad88@gmail.com>
2025-07-04 15:11:11 +0300

    task 07: added create dbDir

--- 07-mvc/implementation/models/messageModel.js
+++ 07-mvc/implementation/models/messageModel.js
@@ -1,9 +1,12 @@
 const path = require('path');
 const fs = require('fs/promises');
 
+const dbDirPath = path.join(__dirname, '..', 'db');
 const dataPath = path.join(__dirname, '..', 'db', 'data.json');
 
 exports.addedMessages = async (username, message) => {
+    await fs.mkdir(dbDirPath, { recursive: true });
+
     const oldData = await fs.readFile(dataPath, 'utf8')
         .then(JSON.parse)
         .catch(() => []);
@@ -15,7 +18,7 @@ exports.addedMessages = async (username, message) => {
         JSON.stringify([newData, ...oldData]),
         'utf8'
     );
-    
+
     return true;
 }
 
