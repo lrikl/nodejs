490a4202aafa698753ec6bc8cd9c3f6515e92c00
Yaroslav <lgalahad88@gmail.com>
2025-07-08 14:26:21 +0300

    task 08-sessions-cookies-auth-passwordless: completed

---
+++ 08-sessions-cookies-auth-passwordless/implementation/app.js
@@ -0,0 +1,93 @@
+require('dotenv').config(); // дозволяє завантажувати змінні оточення зі файлу .env у проект, тим самим приховуючи критичні данні
+const express = require('express');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+const morgan = require('morgan');
+
+const bodyParser = require('body-parser');
+const app = express();
+
+// Налаштування шаблонізатора
+app.set('view engine', 'pug');
+app.set('views', './views');
+// статика
+app.use(express.static(`${__dirname}/assets`));
+
+// Для роботи з кукі
+
+if (!process.env.SESSION_SECRET) {
+  // Викидаємо помилку, якщо немає важливих змінних оточення
+  throw new Error('Error: SESSION_SECRET variable not set in .env file');
+}
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  store: new FileStore({
+    path: './sessions',      // Тека де зберігаються сесії
+    ttl: 3600,               // Час життя сесії у секундах (1 час)
+    retries: 1,              // Спроб запису файлу
+  }),
+  secret: process.env.SESSION_SECRET, // Ваш секрет для кукі. Беремо секрет із змінних оточення
+  resave: false, // не зберігати якщо не змінювалась
+  saveUninitialized: false, // не зберігати в теку якщо не добавляли дані
+  cookie: {
+    maxAge: 3600000          // Час життя кукі у мс (1 час)
+  },
+}));
+
+app.use(morgan('tiny', {
+    skip: (req) => req.url.startsWith('/.well-known'),
+}));
+
+app.use((req, res, next) => {
+  app.locals.username = req.session?.user || null;
+  next();
+});
+
+app.get('/', (req, res)=>{
+  console.log(req.session.user);
+  res.render('main',)
+});
+
+app.get('/common', (req, res)=>{
+  res.render('common')
+});
+
+
+// Сторінка логіну
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+// Обробка форми логіну
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Простий хардкод (можна замінити на базу)
+  if ((username === 'admin' && password === '123') ||
+  (username === 'alex' && password === '777')
+  ) {
+    req.session.user = username;
+    return res.redirect('/dashboard');
+  }
+
+  res.render('login', { error: 'Невірний логін або пароль' });
+});
+
+// Захищена сторінка
+app.get('/dashboard', (req, res) => {
+  if (!req.session.user) return res.redirect('/login');
+  res.render('dashboard', { user: req.session.user });
+});
+
+// Вихід
+app.get('/logout', (req, res) => {
+  req.session.destroy(() => {
+    res.redirect('/login');
+  });
+});
+
+// Запуск
+app.listen(3000, () => {
+  console.log('Сервер запущено на http://localhost:3000');
+});
